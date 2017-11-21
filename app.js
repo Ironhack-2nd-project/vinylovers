2375bdb9f4f9c0d9bf357b2e0504909f46b385b7
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-11-21 17:00:19 +0100

    detalles

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const flash = require('connect-flash');
 //require routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
-const marketplace = require('./routes/marketplace')
-const vinyl = require('./routes/vinyl')
+const marketplace = require('./routes/marketplace');
+const vinyl = require('./routes/vinyl');
+// const user = require('./routes/user');
 
 const app = express();
 
@@ -60,7 +61,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use ('/auth', auth);
 app.use ('/marketplace', marketplace);
-app.use('/vinyl', vinyl);
+// app.use('/vinyl', vinyl);
+// app.use('/user',user);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
