ad94b4c50fb0e66766d57216c7e3a731be446254
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-11-22 15:01:41 +0100

    pequenos cambios

--- routes/vinyl.js
+++ routes/vinyl.js
@@ -11,7 +11,6 @@ router.get('/add', (req, res, next) => {
 });
 
 router.post('/add', upload.single('imgUrl'),(req, res, next) => {
-  console.log(req.user);
   let vinylinfo = {
     albumName: req.body.albumName,
     artistName: req.body.artistName,
