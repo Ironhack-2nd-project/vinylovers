1270cee9cd88bb18bd3b83b91fb5061707159525
Dario Vigil <dariovigil@gmail.com>
2017-11-22 15:56:32 +0100

    added layout

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 // const multer = require('multer')
-// const layouts = require('express-ejs-layouts');
+const layouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
@@ -29,7 +29,8 @@ mongoose.connect(dbURL).then(() => {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.set('layout', 'main-layout');
+app.set('layout', 'layouts/main-layout');
+app.use(layouts);
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
@@ -55,7 +56,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(layouts);
+
 
 
 // Use routes
