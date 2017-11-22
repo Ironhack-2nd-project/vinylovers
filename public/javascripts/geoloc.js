dee5c298c7c32c3b2149fab1a0c03374173e4d29
Dario Vigil <dariovigil@gmail.com>
2017-11-22 17:06:43 +0100

    done

--- public/javascripts/geoloc.js
+++ public/javascripts/geoloc.js
@@ -1,12 +1,18 @@
-var x = document.getElementById("demo");
+// var x = document.getElementById("demo");
 function getLocation() {
+  console.log('dentro getloc');
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        document.getElementById("demo").innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
+    document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
+
+getLocation();
+
+document.getElementById(latitude).value = position.coords.latitude;
+document.getElementById(longitude).value = position.coords.longitude;
